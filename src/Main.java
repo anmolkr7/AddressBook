f9a253b244c11bbcc5478c6c580c4575c699f6fa
anmolkr7 <anmolojha952@gmail.com>
2025-12-12 13:54:16 +0530

    UC1:Added contact class with fields and constructor

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the Address Book Program on Master Branch");
+        Contact contact=new Contact("Anmol", "Ojha", "MG Road",
+                "SomeCity",
+                "SomeState",
+                "123456",
+                "9876543210",
+                "anmol@example.com"
+        );
+        System.out.println(contact);
     }
 }
