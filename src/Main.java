0415d7ac25f62db61f223f0f80ba98a30c21c5dd
anmolkr7 <anmolojha952@gmail.com>
2025-12-12 15:09:54 +0530

    UC2:Running logic to accept contact details until user choice

--- src/Main.java
+++ src/Main.java
@@ -6,40 +6,51 @@ public class Main {
         Scanner sc=new Scanner(System.in);
         AddressBook addressBook=new AddressBook();
 
-        // Taking input from console for UC2
-        System.out.println("\nEnter Contact Details:");
+        boolean isRunning=true;
 
-        System.out.print("First Name: ");
-        String firstName=sc.nextLine();
+        while(isRunning){
+            // Taking input from console for UC2
+            System.out.println("\nEnter Contact Details:");
 
-        System.out.print("Last Name: ");
-        String lastName=sc.nextLine();
+            System.out.print("First Name: ");
+            String firstName = sc.nextLine();
 
-        System.out.print("Address: ");
-        String address=sc.nextLine();
+            System.out.print("Last Name: ");
+            String lastName = sc.nextLine();
 
-        System.out.print("City: ");
-        String city=sc.nextLine();
+            System.out.print("Address: ");
+            String address = sc.nextLine();
 
-        System.out.print("State: ");
-        String state=sc.nextLine();
+            System.out.print("City: ");
+            String city = sc.nextLine();
 
-        System.out.print("Zip: ");
-        String zip=sc.nextLine();
+            System.out.print("State: ");
+            String state = sc.nextLine();
 
-        System.out.print("Phone Number: ");
-        String phone=sc.nextLine();
+            System.out.print("Zip: ");
+            String zip = sc.nextLine();
 
-        System.out.print("Email: ");
-        String email=sc.nextLine();
+            System.out.print("Phone Number: ");
+            String phone = sc.nextLine();
 
-        // Creating new Contact object
-        Contact contact=new Contact(firstName,lastName,address,city,state,zip,phone,email);
+            System.out.print("Email: ");
+            String email = sc.nextLine();
 
-        // Adding to AddressBook
-        addressBook.addContact(contact);
+            // Creating new Contact object
+            Contact contact = new Contact(firstName, lastName, address, city, state, zip, phone, email);
 
-        System.out.println("Your Address Book:");
+            // Adding to AddressBook
+            addressBook.addContact(contact);
+
+            System.out.print("Do you want to add another contact? (yes/no): ");
+            String choice=sc.nextLine().trim().toLowerCase();
+
+            if(!choice.equals("yes")) {
+                isRunning=false;   // exit loop
+            }
+
+        }
+        System.out.println("Address Book:");
         addressBook.showContacts();
     }
 }
